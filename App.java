292a539bd291486122e7890ca91714b73f080233
Bhuvan Raj <163994129+bhuvan-raj@users.noreply.github.com>
2025-12-11 10:07:36 +0530

    Create App.java

---
+++ App.java
@@ -0,0 +1,14 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        // Prints a message to the console
+        System.out.println( "Hello World from Maven!" );
+    }
+}
